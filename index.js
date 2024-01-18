8c81419e9f6661bfc9ad1af11684c6e219136720
benjamin <benjarodasrico@gmail.com>
2024-01-17 22:17:47 -0300

    preentrega3

--- index.js
+++ index.js
@@ -1,33 +1,69 @@
 
-let carrito = 0;
-let continuar;
+const PRODUCTOS = [
 
-const ZAPAS ={
-    1: {precio: 100, stock: true},
-    2: {precio: 200, stock: true},
-    3: {precio: 300, stock: true},
-    4: {precio: 1000000, stock: false},
-    }
+    {id:1, nombre: 'dunk', precio:150, imagen:`./assets/dunk.jpeg`},
+    {id:2, nombre: 'jordan', precio:425, imagen:`./assets/jordan.jpg`},
+    {id:3, nombre:'yeezy', precio:500, imagen:`./assets/yeezy.jpg`}
+];
 
+const carrito = [];
 
-function sumaCarrito(){
-do{
-    let NUMERO = parseFloat(prompt("ingresa el codigo de producto:"));
-    if(NUMERO == 1 || NUMERO == 2 || NUMERO == 3 || NUMERO == 4 && ZAPAS[NUMERO].stock == true){
-        carrito += ZAPAS[NUMERO].precio;
-        continuar = prompt("¿quiere agregar otro producto al carrito?(si/no)").toLowerCase();
-    }
-    else if (ZAPAS[NUMERO].stock == false){
-        alert("este producto no esta en stock");
-        continuar = prompt("¿quiere agregar otro producto al carrito?(si/no)").toLowerCase();
-    }
-    else{
-        alert("ingresa un codigo valido");
-        continuar ="si";
-    }
-} while( continuar === "si");
-alert("el valor total de carrito es: " + carrito);
+//creacion de las cards
+
+function renderizarProductos(){
+    const carritoElement = document.getElementById('carrito');
+    carritoElement.innerHTML = '';
+
+    PRODUCTOS.forEach(producto => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+                    <h3>${producto.nombre}</h3>
+                    <p>precio: $${producto.precio.toFixed(2)} </p>
+                    <img src="${producto.imagen}" alt="">
+                    <button onclick ="agregarAlCarrito(${producto.id})">agregar carrito</button>
+        `;
+        carritoElement.appendChild(card);
+    })
+
 }
 
-sumaCarrito()
+
+//funcion para agregar un producto al carrito
+
+function agregarAlCarrito(idProducto){
+    const productosSeleccionado = PRODUCTOS.find(producto => producto.id === idProducto);
+
+    if(productosSeleccionado){
+        carrito.push(productosSeleccionado);
+        actualizarCarrito();
+    }
+}
+
+//funcion para actualizar y visualizar carrito
+
+function actualizarCarrito(){
+    const carritoElement = document.getElementById('total');
+    carritoElement.innerHTML = '';
+    carrito.forEach(producto =>{
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+                    <h3>${producto.nombre}</h3>
+                    <p>precio: ${producto.precio.toFixed(2)} </p>
+            
+        `;
+        carritoElement.appendChild(card);
+
+    })
+    sumarTotal()
+}
+
+
+function sumarTotal(){
+    const totalElement = document.getElementById('final');
+    const total = carrito.reduce((acc, producto) => acc+ producto.precio,0 )
+    totalElement.innerHTML = `<p> total: $${total}`;
+}
+renderizarProductos();
 
